e7b58d79ab7c93c5ab5a3c8aed2db96aea5ea464
Ilya Paripsa <iparips@gmail.com>
2021-02-10 17:22:09 +1100

    Hook up schema and resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,14 @@
 
 module.exports = {
   Query: {
-    
-  },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
+    pets: (parentResolverValue, args, {models}) => {
+      return models.Pet.findMany({})
     }
   },
-  User: {
-    
+  Pet: {
+    image: (resolvedParentValue) => {
+      console.log("resolvedParentValue: ", resolvedParentValue);
+      return 'image'
+    }
   }
 }
